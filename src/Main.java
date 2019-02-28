1ff258837c18254dc6a7f618e78085c39f5cc168
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-02-28 19:02:24 +0100

    Main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public final class Main {
+	public static void main(final String[] args) throws IOException {
+		final BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+		final BufferedWriter writer = new BufferedWriter(new FileWriter(new File(args[1])));
+		final List<Photo> input = Parser.parse(reader.lines());
+		final List<List<Integer>> output = Solver.solve(input);
+		writer.write(Serializer.serialize(output));
+	}
+}
