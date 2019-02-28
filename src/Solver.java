47a8b330d4fd97a3af28f561b20ca38060353acf
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-02-28 20:20:17 +0100

    Solver

--- src/Solver.java
+++ src/Solver.java
@@ -5,11 +5,32 @@ public final class Solver {
 	}
 
 	public static List<Slide> solve(final List<Photo> photos) {
-		final Map<String, List<Photo>> tags = new HashMap<>();
+		final List<Slide> slides = new ArrayList<Slide>();
+		final List<List<Photo>> order = splitByOrder(photos);
+
+		for (final List<Photo> orderPhotos : order) {
+			slides.addAll(solveSameOrder(orderPhotos));
+		}
+
+		return slides;
+	}
+
+	public static List<Slide> solveSameOrder(final List<Photo> photos) {
+		final Map<String, List<Photo>> tags1 = new HashMap<>();
+		final Map<String, List<Photo>> tags2 = new HashMap<>();
 
 		for (final Photo photo : photos) {
-			for (final String tag : photo.getTags()) {
-				tags.computeIfAbsent(tag, foo -> new ArrayList<>()).add(photo);
+			for (final String tag1 : photo.getTags()) {
+				tags1.computeIfAbsent(tag1, foo -> new ArrayList<>()).add(photo);
+
+				for (final String tag2 : photo.getTags()) {
+					if (tag2 == tag1) {
+						continue;
+					}
+
+					final String tag12 = tag1.compareTo(tag2) > 0 ? tag1 : tag2;
+					tags2.computeIfAbsent(tag12, foo -> new ArrayList<>()).add(photo);
+				}
 			}
 		}
 
