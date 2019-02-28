b1f748b575594028dfa3c3b8952e2f1992679c0f
Fionn Langhans <fionn.langhans@gmail.com>
2019-02-28 18:44:25 +0100

    gitignore

---
+++ src/Parser.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Parser {
+	private Parser() {}
+
+	public static List<List<String>> parse(String[] lines) {
+		// Sanity check
+		assert Integer.parseInt(lines[0]) == lines.length - 1;
+
+		List<List<String>> result = new LinkedList<>();
+		for (int i = 1; i < lines.length; ++i) {
+			
+		}
+	}
+}
