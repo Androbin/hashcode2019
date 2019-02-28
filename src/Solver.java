af34434f5927a03b42936fd3cd5f79c2dbb25b84
Radotornado <radoslav.mandev99@gmail.com>
2019-02-28 20:11:22 +0100

    Added splitByOrder

--- src/Solver.java
+++ src/Solver.java
@@ -15,4 +15,17 @@ public final class Solver {
 
 		return Collections.emptyList();
 	}
+
+	public static List<List<Photo>> splitByOrder(List<Photo> photos) {
+
+		List<List<Photo>> output = new ArrayList<>(100);
+		for (int i = 1; i <= 100; i++) {
+			output.add(new ArrayList<>());
+		}
+		for(Photo photo : photos){
+			output.get(photo.getTags().size()).add(photo);
+		}
+		return output;
+		
+	}
 }
