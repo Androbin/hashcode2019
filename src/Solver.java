441186e0c3139b01fb3b90931c0361a2b2a97ff8
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-03-08 03:16:02 +0100

    Make greedy more efficient

--- src/Solver.java
+++ src/Solver.java
@@ -20,17 +20,24 @@ public final class Solver {
 				.collect(Collectors.toList());
 	}
 
-	private static List<Slide> getVerticalSlides(final List<Photo> photos) {
-		List<Photo> verticalPhotos = photos.stream().filter(Photo::isVertical)
+	private static List<Slide> getVerticalSlides(final List<Photo> photos, final boolean even) {
+		final List<Photo> verticalPhotos = photos.stream().filter(Photo::isVertical)
 				.sorted((photo0, photo1) -> Integer.compare(photo0.getTags().size(), photo1.getTags().size()))
 				.collect(Collectors.toList());
-		// First with second ...
 		final List<Slide> slides = new ArrayList<>(verticalPhotos.size() / 2);
 
-		for (int i = 0; i < verticalPhotos.size() - 1; i += 2) {
-			final Photo first = verticalPhotos.get(i);
-			final Photo second = verticalPhotos.get(i + 1);
-			slides.add(new SlideVertical(first, second));
+		if (even) {
+			for (int i = 0; i < verticalPhotos.size() / 2; i++) {
+				final Photo first = verticalPhotos.get(i);
+				final Photo last = verticalPhotos.get(verticalPhotos.size() - i - 1);
+				slides.add(new SlideVertical(first, last));
+			}
+		} else {
+			for (int i = 0; i < verticalPhotos.size() - 1; i += 2) {
+				final Photo first = verticalPhotos.get(i);
+				final Photo second = verticalPhotos.get(i + 1);
+				slides.add(new SlideVertical(first, second));
+			}
 		}
 
 		return slides;
@@ -39,7 +46,7 @@ public final class Solver {
 	public static List<Slide> solve(final List<Photo> photos) {
 		final List<Slide> slides = new LinkedList<>();
 		slides.addAll(getHorizontalSlides(photos));
-		slides.addAll(getVerticalSlides(photos));
+		slides.addAll(getVerticalSlides(photos, true));
 		return solveSlides(slides);
 	}
 
@@ -58,75 +65,44 @@ public final class Solver {
 		return presentation;
 	}
 
-	// use for dataset B
-	private static List<Slide> solveUniSlides(final List<Slide> slides) {
-		final List<UniSlide> uniSlides = new ArrayList<>();
-		final Map<Slide, List<UniSlide>> map = new HashMap<>();
+	private static List<Slide> solveGreedy(final List<Slide> slides) {
+		final Map<String, List<Slide>> tags1 = new HashMap<>();
 
 		for (final Slide slide : slides) {
-			map.put(slide, new ArrayList<>());
-
 			for (final String tag : slide.getTags()) {
-				final UniSlide us = new UniSlide(slide, tag);
-				uniSlides.add(us);
-				map.get(slide).add(us);
+				tags1.computeIfAbsent(tag, foo -> new ArrayList<>()).add(slide);
 			}
 		}
 
-		uniSlides.sort((s1, s2) -> s1.tag.compareTo(s2.tag));
-
-		while (uniSlides.size() > slides.size()) {
-			System.out.println(uniSlides.size() + "\t" + slides.size());
-
-			for (int i = 0; i < uniSlides.size(); i++) {
-				uniSlides.get(i).score = 0;
-			}
-
-			for (int i = 0; i < uniSlides.size() - 1; i++) {
-				final UniSlide us1 = uniSlides.get(i);
-				final UniSlide us2 = uniSlides.get(i + 1);
-				final int score = slideInterest(us1.slide, us2.slide);
-				us1.score += score;
-				us2.score += score;
-			}
-
-			map.forEach((slide, uniSlides2) -> {
-				uniSlides2.sort((us1, us2) -> -Integer.compare(us1.score, us2.score));
-				final int keep = (uniSlides2.size() + 1) / 2;
-
-				while (uniSlides2.size() > keep) {
-					uniSlides.remove(uniSlides2.remove(uniSlides2.size() - 1));
-				}
-			});
-		}
-
-		final List<Slide> presentation = new ArrayList<>();
-
-		for (final UniSlide uniSlide : uniSlides) {
-			presentation.add(uniSlide.slide);
-		}
-
-		return presentation;
-	}
-
-	private static List<Slide> solveGreedy(final List<Slide> slides) {
 		final List<Slide> presentation = new ArrayList<>(slides.size());
 		final Slide first = slides.remove(0);
 		presentation.add(first);
 		Slide last = first;
 
+		for (final String tag : last.getTags()) {
+			tags1.get(tag).remove(last);
+		}
+
 		while (!slides.isEmpty()) {
+			final List<Slide> reduced = last.getTags().stream().flatMap(tag -> tags1.get(tag).stream()).distinct()
+					.collect(Collectors.toList());
+
 			final Map<Slide, Integer> scores = new HashMap<>();
 
-			for (final Slide slide : slides) {
+			for (final Slide slide : reduced) {
 				scores.put(slide, slideInterest(last, slide));
 			}
 
-			final Slide next = slides.stream().max((a, b) -> Integer.compare(scores.get(a), scores.get(b))).get();
+			final Slide next = reduced.stream().max((a, b) -> Integer.compare(scores.get(a), scores.get(b)))
+					.orElseGet(() -> slides.get(0));
 
 			slides.remove(next);
 			presentation.add(next);
 			last = next;
+
+			for (final String tag : last.getTags()) {
+				tags1.get(tag).remove(last);
+			}
 		}
 
 		System.out.println(score(presentation));
@@ -136,7 +112,7 @@ public final class Solver {
 	// split into buckets ordered by size
 	private static List<List<Slide>> splitByOrder(final List<Slide> slides) {
 		final List<List<Slide>> output = new ArrayList<>();
-		final int spread = 5;
+		final int spread = 1;
 
 		for (int i = 0; i < 100 * spread; i++) {
 			output.add(new ArrayList<>());
@@ -145,20 +121,10 @@ public final class Solver {
 		for (final Slide slide : slides) {
 			final int mod = slide.getPhotos()[0].getIdentifier() % spread;
 			output.get((slide.getTags().size() - 1) * spread + mod).add(slide);
+			// output.get(0).add(slide);
 		}
 
 		return output;
-	}
-
-	private static class UniSlide {
-		public final Slide slide;
-		public final String tag;
-		public int score;
-
-		public UniSlide(final Slide slide, final String tag) {
-			this.slide = slide;
-			this.tag = tag;
-		}
 	}
 
 	public static int score(final List<Slide> slides) {
